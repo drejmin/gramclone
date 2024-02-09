7e87b169c4180322a4ec06fb73d64a9224534732
drejmin <djjamn@gmail.com>
2024-02-09 13:47:33 -0800

    new file:   @/components/ui/button.tsx 	new file:   @/lib/utils.ts 	new file:   app/globals.css 	new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	deleted:    public/vite.svg 	new file:   src/App.tsx 	new file:   src/_auth/AuthLayout.tsx 	new file:   src/_auth/forms/SignUpForm.tsx 	new file:   src/_auth/forms/SigninForm.tsx 	new file:   src/_root/RootLayout.tsx 	new file:   src/_root/pages/Home.tsx 	new file:   src/_root/pages/index.ts 	new file:   src/globals.css 	modified:   src/main.tsx 	new file:   tailwind.config.js 	modified:   tsconfig.node.json 	modified:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import AuthLayout from './_auth/AuthLayout';
+import SignUpForm from './_auth/forms/SignUpForm';
+import SigninForm from './_auth/forms/SigninForm';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* {public routes} */}
+            <Route element={<AuthLayout/>} >
+                <Route path= "/sign-in" element={<SigninForm/>} />
+                <Route path= "/sign-un" element={<SignUpForm/>} />
+            </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../../../@/components/ui/button.tsx";
+
+
+const SignUpForm = () => {
+  return (
+    <div>
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default SignUpForm
