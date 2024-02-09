7189b90e3d878515b0b61c0ace7eeb9121c77f96
drejmin <djjamn@gmail.com>
2024-02-09 13:55:55 -0800

    modified:   src/_auth/AuthLayout.tsx

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,10 @@ const AuthLayout = () => {
             <Outlet/>
           </section>
 
-          <img />
+          <img 
+            src='assets/images/side-img.svg'
+            alt = 'logo'
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'/>
         </>
       )}
     </>
