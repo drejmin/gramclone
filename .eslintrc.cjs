37b36325e6191ed5e15c139f802191327dd32bd1
drejmin <djjamn@gmail.com>
2024-02-09 16:59:01 -0800

    modified:   .eslintrc.cjs 	new file:   app/globals.css 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	modified:   src/_auth/AuthLayout.tsx 	modified:   src/_auth/forms/SignUpForm.tsx 	modified:   src/globals.css 	modified:   tailwind.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
+    "standard",
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:tailwindcss/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import SignUpForm from './_auth/forms/SignUpForm';
 import SigninForm from './_auth/forms/SigninForm';
 import RootLayout from './_root/RootLayout';
 import { Home } from './_root/pages';
-import './globals.css';
 import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
             {/* {public routes} */}
             <Route element={<AuthLayout/>} >
                 <Route path= "/sign-in" element={<SigninForm/>} />
-                <Route path= "/sign-un" element={<SignUpForm/>} />
+                <Route path= "/sign-up" element={<SignUpForm/>} />
             </Route>
 
             {/* private routes */}
