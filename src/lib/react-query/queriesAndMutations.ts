c7f1667ae926130c3baebab25b4700c1fb26da8f
drejmin <djjamn@gmail.com>
2024-02-20 16:20:47 -0800

    modified:   src/context/AuthContext.tsx 	modified:   src/lib/appwrite/api.ts 	modified:   src/lib/react-query/queriesAndMutations.ts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -16,7 +16,7 @@ export const useSignInAccount = () => {
     return useMutation({
         mutationFn: (user: {
             email: string;
-             password: string;
+            password: string;
             }) => signInAccount(user)
     })
 }
