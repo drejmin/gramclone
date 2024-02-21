2c576926ce18a4797e6260b94a7b48db02c8ad58
drejmin <djjamn@gmail.com>
2024-02-20 19:28:25 -0800

    modified:   src/App.tsx 	modified:   src/_auth/forms/SigninForm.tsx 	modified:   src/_root/RootLayout.tsx 	new file:   src/_root/pages/AllUsers.tsx 	new file:   src/_root/pages/CreatePost.tsx 	new file:   src/_root/pages/EditPost.tsx 	new file:   src/_root/pages/Explore.tsx 	new file:   src/_root/pages/LikedPost.tsx 	new file:   src/_root/pages/PostDetails.tsx 	new file:   src/_root/pages/Profile.tsx 	new file:   src/_root/pages/Saved.tsx 	new file:   src/_root/pages/UpdateProfile.tsx 	modified:   src/_root/pages/index.ts 	new file:   src/components/ui/shared/Bottombar.tsx 	new file:   src/components/ui/shared/LeftSidebar.tsx 	new file:   src/components/ui/shared/Topbar.tsx 	new file:   src/constants/index.ts 	modified:   src/lib/appwrite/api.ts 	modified:   src/lib/react-query/queriesAndMutations.ts 	modified:   src/lib/validation/index.ts

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import SigninForm from './_auth/forms/SigninForm';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from "@/components/ui/toaster"
 
-import { Home } from './_root/pages';
+import { AllUsers, EditPost, Explore, Home, LikedPost, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 
 import './globals.css';
+import CreatePost from './_root/pages/CreatePost';
 
 const App = () => {
   return (
@@ -21,8 +22,17 @@ const App = () => {
             </Route>
 
             {/* private routes */}
-            <Route element={<RootLayout />}>
+            < Route element={<RootLayout />}>
                 <Route index element={<Home />} />
+                <Route path='/explore' element={<Explore/>}/>
+                <Route path='/saved' element={<Saved/>}/>
+                <Route path='/all-users' element={<AllUsers/>}/>
+                <Route path='/create-post' element={<CreatePost/>}/>
+                <Route path='/update-post/:id' element={<EditPost/>}/>
+                <Route path='/posts/:id' element={<PostDetails/>}/>
+                <Route path='/profile/:id/*' element={<Profile/>}/>
+                <Route path='/update-profile/:id' element={<UpdateProfile/>}/>
+                <Route path='/liked-post' element={<LikedPost/>}/>
             </Route>
         </Routes>
       <Toaster/>
